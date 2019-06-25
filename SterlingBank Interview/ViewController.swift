7f0361b99b1e5dae3b44f6ad5d034f980051226f
SimpuMind <afees@hollaport.net>
2019-06-25 14:44:42 +0100

    Initial Commit

---
+++ SterlingBank Interview/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SterlingBank Interview
+//
+//  Created by Afees Lawal on 6/25/19.
+//  Copyright © 2019 Afees Lawal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
