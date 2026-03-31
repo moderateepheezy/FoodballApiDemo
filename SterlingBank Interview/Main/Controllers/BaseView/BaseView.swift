5652bb1a9db7e2d1399542085088e48752b8edcb
Afees Lawal <moderateepheezy@gmail.com>
2026-03-31 18:42:28 +0200

    Add SwiftUI setup to BaseView

--- SterlingBank Interview/Main/Controllers/BaseView/BaseView.swift
+++ SterlingBank Interview/Main/Controllers/BaseView/BaseView.swift
@@ -1,16 +1,14 @@
+// BaseView.swift
+// SterlingBank Interview
 //
-//  BaseView.swift
-//  SterlingBank Interview
-//
-//  Created by Afees Lawal on 6/25/19.
-//  Copyright © 2019 Afees Lawal. All rights reserved.
-//
+// Created by Afees Lawal on 6/25/19.
+// Copyright © 2019 Afees Lawal. All rights reserved.
 
 import UIKit
 
 class BaseView: UIView {
     
-    override init(frame: CGRect) {
+override init(frame: CGRect) {
         super.init(frame: frame)
         backgroundColor = .white
         configure()
@@ -21,6 +19,17 @@ class BaseView: UIView {
     }
     
     public func configure() {
-        
+        // Setup for SwiftUI integration can be added here
+        let swiftUIView = YourSwiftUIView() // Replace with your actual SwiftUI View
+        let hostingController = UIHostingController(rootView: swiftUIView)
+        hostingController.view.frame = self.bounds
+        addSubview(hostingController.view)
+        hostingController.view.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            hostingController.view.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+            hostingController.view.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+            hostingController.view.topAnchor.constraint(equalTo: self.topAnchor),
+            hostingController.view.bottomAnchor.constraint(equalTo: self.bottomAnchor)
+        ])
     }
 }
